d850fabd0429a51ff463978b98ae232c84d1eab3
Linh Hoang-Tuan <linhht.419@gmail.com>
2023-11-20 16:40:06 +0900

    Initial commit for the user traffic model

--- traffic_model.py
+++ traffic_model.py
@@ -26,15 +26,17 @@ def generate_traffic_ON_OFF(n_slots=1000,
     traffic_state[0] = ON if rng.uniform() <= ON_duration_mean_tslot/(ON_duration_mean_tslot + OFF_duration_mean_tslot) else OFF 
     
     n_types = len(data_arrival_mean_Mb)             # no. of service types 
+    traffic_type_fixed = np.random.choice(n_types)
+    
     traffic_type = np.full(shape=(n_slots), fill_value=-1, dtype=int)       # -1 indicates the OFF state 
-    traffic_type[0] = np.random.choice(n_types) if traffic_state[0] == ON else -1 
+    traffic_type[0] = traffic_type_fixed if traffic_state[0] == ON else -1 
     
     traffic_mu = np.zeros(shape=(n_slots))          # mean of arrival traffic over time, corresponding to each service type 
     traffic_mu[0] = data_arrival_mean_Mb[traffic_type[0]] if traffic_state[0] == ON else 0 
     
     t_start = 0; t_stop = 0
-    n_samples_exponential = 5
-    n_samples_pareto = 5 
+    n_samples_exponential = 1
+    n_samples_pareto = 1
     
     while t_stop < n_slots:
         
@@ -53,8 +55,8 @@ def generate_traffic_ON_OFF(n_slots=1000,
         # Update t_start 
         if t_stop < n_slots: 
             t_start = t_stop 
-            traffic_state[t_start] = ~np.array(traffic_state[t_stop-1], dtype=bool)
-            traffic_type[t_start] = np.random.choice(n_types) if traffic_state[t_start] == ON else -1 
+            traffic_state[t_start] = ~np.array(traffic_state[t_stop-1], dtype=bool)     # revert the traffic state
+            traffic_type[t_start] = traffic_type_fixed if traffic_state[t_start] == ON else -1 
             traffic_mu[t_start] = data_arrival_mean_Mb[traffic_type[t_start]] if traffic_state[t_start] == ON else 0
     
     
@@ -68,7 +70,7 @@ def generate_traffic_ON_OFF(n_slots=1000,
     traffic_arrival = traffic_arrival * traffic_state           # shape=(n_slots,)
     
     # bounded Pareto 
-    bound = 5*traffic_mu
+    bound = 3*traffic_mu
     traffic_arrival = np.where(traffic_arrival <= bound, traffic_arrival, bound)
     
     return traffic_state, traffic_arrival, traffic_type 
